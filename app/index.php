0581e420a12606b5d1e1be3d9eaa572f746a0d77
jpdrsanchez <joaopedrosanchez302@gmail.com>
2023-12-20 12:45:13 -0300

    chore/wis-69: testes de log da aplicação

--- app/index.php
+++ app/index.php
@@ -1,6 +1,12 @@
 <?php
 
+use Dotenv\Dotenv;
+use Studio\Bridge\Bridge;
+
 require_once __DIR__ . '/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
+
+$bridge = new Bridge();
+$bridge->read();
---
+++ app/src/Bridge.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Studio\Bridge;
+
+class Bridge
+{
+    public function __construct()
+    {
+    }
+
+    public function read(): void
+    {
+        if ($json = json_decode(file_get_contents("php://input"), true)) {
+            $data = $json;
+        } else {
+            $data = $_POST;
+        }
+
+        $file = fopen(__DIR__ . '/log.txt', 'a+');
+
+        if (! $file) {
+            return;
+        }
+        $now = date('c');
+        fputs($file, "[$now]: $data \n");
+        fclose($file);
+    }
+}
