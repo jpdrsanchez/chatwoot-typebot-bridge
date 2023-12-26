5318bbd46847037c978c2b593717a83dc028a320
jpdrsanchez <joaopedrosanchez302@gmail.com>
2023-12-26 15:33:31 -0300

    fix/wis-69: ajuste na váriavel que traz o token do agent bot do chatwoot

--- app/src/Usecases/SendMessageToChatwoot.php
+++ app/src/Usecases/SendMessageToChatwoot.php
@@ -21,15 +21,15 @@ class SendMessageToChatwoot
         $chatwoot_url    = $query->chatwoot_url;
         $account_id      = $chatwoot->account?->id;
         $conversation_id = $chatwoot->conversation?->id;
-        $bot_token       = $chatwoot->chatwoot_bot_token;
+        $bot_token       = $query->chatwoot_bot_token;
 
         $url     =
             "$chatwoot_url/api/v1/accounts/$account_id/conversations/$conversation_id/messages";
-        $body    = [
+        $body    = json_encode([
             'content'      => $message,
             'message_type' => 'outgoing',
             'private'      => true
-        ];
+        ]);
         $headers =
             "Content-Type: application/json\r\nAccept: application/json\r\napi_access_token: $bot_token";
         $options = [
