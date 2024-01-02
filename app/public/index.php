b69bf68014fa798cc07ef36793628243e573147b
jpdrsanchez <joaopedrosanchez302@gmail.com>
2024-01-02 16:45:29 -0300

    feature/wis-69: create form link generator

--- app/public/index.php
+++ app/public/index.php
@@ -2,6 +2,8 @@
 
 use Dotenv\Dotenv;
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 use Studio\Bridge\Controllers\WebhookController;
 
 require __DIR__ . '/../vendor/autoload.php';
@@ -11,8 +13,16 @@ $dotenv->safeLoad();
 
 $app = AppFactory::create();
 
+$twig = Twig::create('../views', [ 'cache' => false ]);
+
 $app->post('/webhook', [ WebhookController::class, 'index' ]);
+$app->get('/form', function ($request, $response, $args) {
+    $view = Twig::fromRequest($request);
+
+    return $view->render($response, 'form.html');
+});
 
 $app->addErrorMiddleware(true, true, true);
+$app->add(TwigMiddleware::create($app, $twig));
 
 $app->run();
